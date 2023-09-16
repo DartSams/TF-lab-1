15f3712e9b0725e09b49c8990c87402588bdb425
dartsams <dartagnansams1@icloud.com>
2023-09-16 01:32:30 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Cool Project</h1>
+      <h2>Here is my schedule.</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
